9c2dab8af8e9bd90a3a172b3727b152cd427c2ed
Om Wankhede <omwankhede369@gmail.com>
2024-09-26 17:50:54 +0530

    1st commit
    ist commit

---
+++ 5-Importing,Creating Modules and PAckages/package/__init__.py
@@ -0,0 +1,3 @@
+'''
+__init__.py is a special file used in python to define Packages and initialize their namespaces 
+'''
---
+++ 5-Importing,Creating Modules and PAckages/test.py
@@ -0,0 +1,6 @@
+from package.maths import *
+from package.subpackages.mult import multiplication
+
+print(addition(2,3))
+print(substraction(4,3))
+print(multiplication(4,5))
